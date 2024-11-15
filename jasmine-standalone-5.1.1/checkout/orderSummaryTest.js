6b74c76503fd421b6579141d11a3ac1afdf3c2b4
KaysonSouksaly <kayson.souksaly@gmail.com>
2024-11-15 15:23:56 -0500

    fixed the problem

---
+++ jasmine-standalone-5.1.1/checkout/orderSummaryTest.js
@@ -0,0 +1,37 @@
+import { renderOrderSummary } from '../../scripts/checkout/orderSummary.js';
+import { cart, loadFromStorage } from '../../data/cart.js';
+
+describe('test suite: renderOrderSummary', () => {
+    it('displays the cart', () => {
+        // Set up inner HTML for the test container
+        document.querySelector('.js-test-container').innerHTML = `
+            <div class="js-order-summary"></div>
+        `;
+        
+        spyOn(localStorage, 'setItem');
+
+        // Mock localStorage to return a predefined cart
+        spyOn(localStorage, 'getItem').and.callFake(() => {
+            return JSON.stringify([
+                {
+                    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                    quantity: 2,
+                    deliveryOptionId: '1',
+                },
+                {
+                    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+                    quantity: 1,
+                    deliveryOptionId: '2',
+                },
+            ]);
+        });
+        loadFromStorage();
+
+        renderOrderSummary();
+        console.log(document.querySelectorAll('.js-cart-item-container'))
+
+        expect(
+            document.querySelectorAll('.js-cart-item-container').length
+        ).toEqual(2);
+    });
+});
