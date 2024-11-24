9ab46c67831f454183136c32bfe0badf25a1391c
KaysonSouksaly <kayson.souksaly@gmail.com>
2024-11-24 00:45:33 -0500

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -76,6 +76,32 @@ class Clothing extends Product {
 // }
 // object3.mehtod()
 
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      
+        return new Product(productDetails);
+      });
+
+      console.log('load products');
+
+      fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -742,3 +768,4 @@ if (productDetails.type === 'clothing') {
 
   return new Product(productDetails);
 });
+*/
